343af8ac562d3832d4b38ea957d9296775e44a6a
IGOR DO NASCIMENTO ALVES <unifigalves@fei.edu.br>
2019-11-05 22:33:58 -0300

    calc dist

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-print("Hello Word")
+import calc_dist as CD
 arquivo = open('mapa250.txt', 'r')
 stream = arquivo.readlines()
 lines = []
 for line in stream:
     lines.append(line.split())
-print(lines)
+teste1 =  CD.Teste()
+teste1.funcTeste([lines[0], lines[1], lines[2]])
 arquivo.close()
